0f813ad44d675ed835adfbc3852c65b1a96c957d
José Caldas <jrcaldas@yahoo.com.br>
2021-04-02 14:09:03 -0300

    add checkbox component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import AppContainer from "../../AppContainer/AppContainer";
 import AppHeader from "../AppHeader/AppHeader";
+import Checkbox from "../shared/Checkbox";
 import "./AppStyle";
 import { Container, Wrapper } from "./AppStyle";
 
 function App() {
+  const [lettuce, setLettuce] = useState(true);
+  const [rice, setRice] = useState(false);
+
   return (
     <Wrapper>
       <Container>
         <AppHeader />
         <AppContainer
-          left={<div>Produtos Disponíveis</div>}
-          middle={<div>Sua Lista de Compras</div>}
-          right={<div>Estatísticas</div>}
+          left={
+            <div>
+              Produtos Disponíveis:
+              <Checkbox
+                title="Alface"
+                value={lettuce}
+                onClick={() => {
+                  setLettuce(!lettuce);
+                }}
+              />
+              <Checkbox
+                title="Arroz"
+                value={rice}
+                onClick={() => {
+                  setRice(!rice);
+                }}
+              />
+            </div>
+          }
+          middle={<div>Sua Lista de Compras:</div>}
+          right={<div>Estatísticas:</div>}
         />
       </Container>
     </Wrapper>
