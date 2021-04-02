1412dc71621a695a98d4606d0acfedcf75fad82c
José Caldas <jrcaldas@yahoo.com.br>
2021-04-02 15:41:16 -0300

    add LineChart component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppContainer from "../../AppContainer/AppContainer";
 import AppHeader from "../AppHeader/AppHeader";
-import Checkbox from "../shared/Checkbox";
+import Checkbox from "../shared/Checkbox/Checkbox";
+import LineChart from "../shared/LineChart/LineChart";
 import "./AppStyle";
 import { Container, Wrapper } from "./AppStyle";
 
 function App() {
   const [lettuce, setLettuce] = useState(true);
   const [rice, setRice] = useState(false);
+
+  const [healthy, setHealthy] = useState(5);
+
+  const colors = ["#62cbc6", "#00ABAD", " #00858C ", "#006073", "#004d61"];
+  // const colors = ["#62CBC6​", "#00ABAD​", "#00858C​", "#006073​", "#004D61​"];:
+
+  useEffect(() => {}, []);
+  setTimeout(() => {
+    setHealthy(80);
+  }, 1000);
 
   return (
     <Wrapper>
@@ -34,7 +45,31 @@ function App() {
             </div>
           }
           middle={<div>Sua Lista de Compras:</div>}
-          right={<div>Estatísticas:</div>}
+          right={
+            <div>
+              Estatísticas:
+              <LineChart
+                color={colors[0]}
+                title="saudável"
+                percentage={healthy}
+              />
+              <LineChart
+                color={colors[1]}
+                title="não tão saudável assim"
+                percentage={healthy}
+              />
+              <LineChart
+                color={colors[2]}
+                title="Limpeza"
+                percentage={healthy}
+              />
+              <LineChart
+                color={colors[3]}
+                title="Outros"
+                percentage={healthy}
+              />
+            </div>
+          }
         />
       </Container>
     </Wrapper>
---
+++ src/components/shared/LineChart/LineChart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Wrapper, ProgressBar } from "./LineChartStyle";
+
+function LineChart({ title, percentage, color }) {
+  return (
+    <Wrapper>
+      <span>{title}:</span>
+      <ProgressBar percentage={percentage} color={color} />
+    </Wrapper>
+  );
+}
+
+export default LineChart;
